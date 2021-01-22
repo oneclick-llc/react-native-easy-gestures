a0d5a94db290b7a7bc90d7c5adbf6d6f2434927b
ewlkkf <craftedmovie30@gmail.com>
2021-01-22 10:06:27 +0900

    type added & feature added

---
+++ lib/index.d.ts
@@ -0,0 +1,37 @@
+import React from "react";
+import { AccessibilityProps, StyleProp, ViewStyle } from "react-native";
+
+declare module "react-native-easy-gestures";
+
+export interface GesturesProps extends AccessibilityProps {
+  draggable?: boolean | { x?: boolean; y?: boolean };
+  rotatable?: boolean | { step?: number };
+  scalable?: boolean | { min?: number; max?: number };
+  rotate?: string;
+  scale?: number;
+
+  style?: StyleProp<ViewStyle>;
+
+  onStart?: (event: object, styles: object) => void;
+  onChange?(event: object, styles: object): void;
+  onEnd?(event: object, styles: object): void;
+  onMultyTouchStart?(event: object, styles: object): void;
+  onMultyTouchChange?(event: object, styles: object): void;
+  onMultyTouchEnd?(event: object, styles: object): void;
+  onRotateStart?(event: object, styles: object): void;
+  onRotateChange?(event: object, styles: object): void;
+  onRotateEnd?(event: object, styles: object): void;
+  onScaleStart?(event: object, styles: object): void;
+  onScaleChange?(event: object, styles: object): void;
+  onScaleEnd?(event: object, styles: object): void;
+}
+
+interface GesturesStaticProperties {
+  draggable?: true | { x?: boolean; y?: boolean };
+  rotatable?: boolean;
+  scalable?: boolean | { min?: number; max?: number };
+}
+
+declare const Gestures: React.ComponentType<GesturesProps> &
+  GesturesStaticProperties;
+export default Gestures;
